7a45834f75aa4e0a4abbdc5d09d123331a14fc4b
Hodor <geoffrey.aulombard@gmail.com>
2023-11-26 19:43:33 +0100

    continue tuto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,14 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
-  mySnap!: FaceSnap;
-  mySnap1!: FaceSnap;
-  mySnap2!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap("Pouet", "C'est super cool", new Date(), 0, "https://cdn.pixabay.com/photo/2023/11/16/22/06/fritillary-butterfly-8393205_640.jpg", "Paris")
-    this.mySnap1 = new FaceSnap("Bird", "C'est super cool", new Date(), 0, "https://cdn.pixabay.com/photo/2023/10/23/17/53/bird-8336583_640.jpg")
-    this.mySnap2 = new FaceSnap("Sloubi", "C'est super cool", new Date(), 0, "https://cdn.pixabay.com/photo/2023/11/13/18/09/forest-8386084_640.jpg")
+    this.faceSnaps = [
+      new FaceSnap("L'effet papillon", "C'est super cool", new Date(), 0, "https://cdn.pixabay.com/photo/2023/11/16/22/06/fritillary-butterfly-8393205_640.jpg", "Paris"),
+      new FaceSnap("Birdy", "Trop mimi", new Date(), 350, "https://cdn.pixabay.com/photo/2023/10/23/17/53/bird-8336583_640.jpg"),
+      new FaceSnap("Chant d'Sloubi", "C'est super fun ici !", new Date(), 0, "https://cdn.pixabay.com/photo/2023/11/13/18/09/forest-8386084_640.jpg", "kaamelot")
+    ]
     throw new Error('Method not implemented.');
   }
 
