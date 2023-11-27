b48a085540e4fdab0c0ace54c99965ab34b766e8
Hodor <geoffrey.aulombard@gmail.com>
2023-11-27 10:08:23 +0100

    tuto

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+
+  constructor(private router: Router) { }
+
+  onContinue() {
+    this.router.navigateByUrl('facesnaps')
+  }
 
 }
