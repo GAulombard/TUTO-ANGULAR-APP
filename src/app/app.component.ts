76a9cb26183fce3ae4e5d72e8a1ae102791dca31
Hodor <geoffrey.aulombard@gmail.com>
2023-11-27 18:22:21 +0100

    tuto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+import { Observable, filter, interval, map, tap } from 'rxjs';
 
 
 
@@ -13,4 +14,19 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  interval$!: Observable<string>;
+
+  ngOnInit(): void {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ? 'pair : ' + value : 'impair : ' + value),
+      tap(text => this.logger(text))
+    );
+  }
+
+  logger(text: string) {
+    console.log(`log : ${text}`);
+  }
+}
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { Subject, interval, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -11,13 +12,25 @@ import { FaceSnapsService } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.css'
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   constructor(private faceSnapsService: FaceSnapsService) { }
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(value => console.log(value))
+    ).subscribe();
   }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+  }
+
 }
