ade6a3b772b384529c20190a0434fb62ba1f9e2f
Hodor <geoffrey.aulombard@gmail.com>
2023-11-27 23:53:38 +0100

    tuto

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -9,9 +9,15 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private auth: AuthService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const headers = new HttpHeaders()
-            .append('Authorization', `Bearer ${this.auth.getToken()}`);
-        const modifiedReq = req.clone({ headers });
-        return next.handle(modifiedReq);
+        const authToken = this.auth.getToken();
+
+        // Clone the request and replace the original headers with
+        // cloned headers, updated with the authorization.
+        const authReq = req.clone({
+            headers: req.headers.set('Authorization', authToken)
+        });
+
+        // send cloned request with header to the next handler.
+        return next.handle(authReq);
     }
 }
